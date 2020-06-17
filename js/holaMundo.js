c23c73ed11b81e9582d65a6433b8b2ac86b3f81b
Luciano Gramajo <lucianongramajo@gmail.com>
2020-06-17 00:45:59 -0300

    algunos arreglos

--- js/holaMundo.js
+++ js/holaMundo.js
@@ -1,5 +1,62 @@
-function digitos(){
-    console.log("mostrar numero");
+document.getElementById("one");
+document.getElementById("twoo");
+document.getElementById("three");
+document.getElementById("four");
+document.getElementById("five");
+document.getElementById("six");
+document.getElementById("seven");
+document.getElementById("eight");
+document.getElementById("nine");
+document.getElementById("zero");
 
-    console.innerText("7")
+function numeros() {
+    let actual = document.getElementById("resultado").innerHTML;
+    let sumado = document.getElementById("one").innerHTML;
+    document.getElementById("resultado").innerHTML = actual + sumado;
+}
+
+function numeros() {
+    let actual = document.getElementById("resultado").innerHTML;
+    let sumado = document.getElementById("twoo").innerHTML;
+    document.getElementById("resultado").innerHTML = actual + sumado;
+}
+function numeros() {
+    let actual = document.getElementById("resultado").innerHTML;
+    let sumado = document.getElementById("three").innerHTML;
+    document.getElementById("resultado").innerHTML = actual + sumado;
+}
+function numeros() {
+    let actual = document.getElementById("resultado").innerHTML;
+    let sumado = document.getElementById("four").innerHTML;
+    document.getElementById("resultado").innerHTML = actual + sumado;
+}
+function numeros() {
+    let actual = document.getElementById("resultado").innerHTML;
+    let sumado = document.getElementById("five").innerHTML;
+    document.getElementById("resultado").innerHTML = actual + sumado;
+}
+function numeros() {
+    let actual = document.getElementById("resultado").innerHTML;
+    let sumado = document.getElementById("six").innerHTML;
+    document.getElementById("resultado").innerHTML = actual + sumado;
+}
+function numeros() {
+    let actual = document.getElementById("resultado").innerHTML;
+    let sumado = document.getElementById("seven").innerHTML;
+    document.getElementById("resultado").innerHTML = actual + sumado;
+}
+function numeros() {
+    let actual = document.getElementById("resultado").innerHTML;
+    let sumado = document.getElementById("eight").innerHTML;
+    document.getElementById("resultado").innerHTML = actual + sumado;
+}
+function numeros() {
+    let actual = document.getElementById("resultado").innerHTML;
+    let sumado = document.getElementById("nine").innerHTML;
+    document.getElementById("resultado").innerHTML = actual + sumado;
+}
+function numeros() {
+    let actual = document.getElementById("resultado").innerHTML;
+    let sumado = document.getElementById("zero").innerHTML;
+    document.getElementById("resultado").innerHTML = actual + sumado;
 }
