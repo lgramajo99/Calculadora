1e9f9926a707bed9f10c94de8b53b449ee989404
Luciano Gramajo <lucianongramajo@gmail.com>
2020-06-20 22:42:22 -0300

    se agregan numeros al indicador

---
+++ js/holaMundo.js
@@ -0,0 +1,61 @@
+function n1() {
+    console.log("hola apretaste el n1");
+    let numeroX = 1;
+    let actual = document.getElementById("demo").innerHTML;
+    document.getElementById("demo").innerHTML = actual + numeroX;
+}
+
+function n2() {
+    console.log("hola apretaste el n2");
+    let numeroX = 2;
+    let actual = document.getElementById("demo").innerHTML;
+    document.getElementById("demo").innerHTML = actual + numeroX;
+}
+function n3() {
+    console.log("hola apretaste el n3");
+    let numeroX = 3;
+    let actual = document.getElementById("demo").innerHTML;
+    document.getElementById("demo").innerHTML = actual + numeroX;
+}
+function n4() {
+    console.log("hola apretaste el n4");
+    let numeroX = 4;
+    let actual = document.getElementById("demo").innerHTML;
+    document.getElementById("demo").innerHTML = actual + numeroX;
+}
+function n5() {
+    console.log("hola apretaste el n5");
+    let numeroX = 5;
+    let actual = document.getElementById("demo").innerHTML;
+    document.getElementById("demo").innerHTML = actual + numeroX;
+}
+function n6() {
+    console.log("hola apretaste el n6");
+    let numeroX = 6;
+    let actual = document.getElementById("demo").innerHTML;
+    document.getElementById("demo").innerHTML = actual + numeroX;
+}
+function n7() {
+    console.log("hola apretaste el n7");
+    let numeroX = 7;
+    let actual = document.getElementById("demo").innerHTML;
+    document.getElementById("demo").innerHTML = actual + numeroX;
+}
+function n8() {
+    console.log("hola apretaste el n8");
+    let numeroX = 8;
+    let actual = document.getElementById("demo").innerHTML;
+    document.getElementById("demo").innerHTML = actual + numeroX;
+}
+function n9() {
+    console.log("hola apretaste el n9");
+    let numeroX = 9;
+    let actual = document.getElementById("demo").innerHTML;
+    document.getElementById("demo").innerHTML = actual + numeroX;
+}
+function n0() {
+    console.log("hola apretaste el n0");
+    let numeroX = 0;
+    let actual = document.getElementById("demo").innerHTML;
+    document.getElementById("demo").innerHTML = actual + numeroX;
+}
