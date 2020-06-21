7597a84eea0b319e16eb3df26009f232c71ab343
Luciano Gramajo <lucianongramajo@gmail.com>
2020-06-21 03:48:17 -0300

    se hacen unos arreglos en js

--- js/holaMundo.js
+++ js/holaMundo.js
@@ -1,86 +1,96 @@
+let a1 = 1;
+let a2 = 2;
+let a3 = 3;
+let a4 = 4;
+let a5 = 5;
+let a6 = 6;
+let a7 = 7;
+let a8 = 8;
+let a9 = 9;
+let a0 = 0;
+
 function n1() {
     console.log("hola apretaste el n1");
-    let numeroX = 1;
+    // let numeroX = 1;
     let actual = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = actual + numeroX;
+    document.getElementById("demo").innerHTML = actual + a1;
 }
-
 function n2() {
     console.log("hola apretaste el n2");
-    let numeroX = 2;
+    // let a2 = 2;
     let actual = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = actual + numeroX;
+    document.getElementById("demo").innerHTML = actual + a2;
 }
 function n3() {
     console.log("hola apretaste el n3");
-    let numeroX = 3;
+    // let numeroX = 3;
     let actual = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = actual + numeroX;
+    document.getElementById("demo").innerHTML = actual + a3;
 }
 function n4() {
     console.log("hola apretaste el n4");
-    let numeroX = 4;
+    // let numeroX = 4;
     let actual = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = actual + numeroX;
+    document.getElementById("demo").innerHTML = actual + a4;
 }
 function n5() {
     console.log("hola apretaste el n5");
-    let numeroX = 5;
+    // let numeroX = 5;
     let actual = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = actual + numeroX;
+    document.getElementById("demo").innerHTML = actual + a5;
 }
 function n6() {
     console.log("hola apretaste el n6");
-    let numeroX = 6;
+    // let numeroX = 6;
     let actual = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = actual + numeroX;
+    document.getElementById("demo").innerHTML = actual + a6;
 }
 function n7() {
     console.log("hola apretaste el n7");
-    let numeroX = 7;
+    // let numeroX = 7;
     let actual = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = actual + numeroX;
+    document.getElementById("demo").innerHTML = actual + a7;
 }
 function n8() {
     console.log("hola apretaste el n8");
-    let numeroX = 8;
+    // let numeroX = 8;
     let actual = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = actual + numeroX;
+    document.getElementById("demo").innerHTML = actual + a8;
 }
 function n9() {
     console.log("hola apretaste el n9");
-    let numeroX = 9;
+    // let numeroX = 9;
     let actual = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = actual + numeroX;
+    document.getElementById("demo").innerHTML = actual + a9;
 }
 function n0() {
     console.log("hola apretaste el n0");
-    let numeroX = 0;
+    // let numeroX = 0;
     let actual = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = actual + numeroX;
+    document.getElementById("demo").innerHTML = actual + a0;
 }
 
 function opD() {
     console.log("hola apretaste la division");
     let operacion = "/";
     let actual = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = actual + " " + operacion + " ";
+    document.getElementById("demo").innerHTML = actual + " " + operacion;
 }
 function opM() {
     console.log("hola apretaste la mulsiplicacion");
     let operacion = "*";
     let actual = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = actual + " " + operacion + " ";
+    document.getElementById("demo").innerHTML = actual + " " + operacion;
 }
 function opR() {
     console.log("hola apretaste la resta");
     let operacion = "-";
     let actual = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = actual + " " + operacion + " ";
+    document.getElementById("demo").innerHTML = actual + " " + operacion;
 }
 function opS() {
     console.log("hola apretaste la suma");
     let operacion = "+";
     let actual = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = actual + " " + operacion + " ";
+    document.getElementById("demo").innerHTML = actual + " " + operacion;
 }
