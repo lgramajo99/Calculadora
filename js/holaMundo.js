31b45ab6ba980f2ea4764e6a5e990aa1181f2734
Luciano Gramajo <lucianongramajo@gmail.com>
2020-06-26 01:20:21 -0300

    se crea base completa de js

--- js/holaMundo.js
+++ js/holaMundo.js
@@ -1,96 +1,129 @@
-let a1 = 1;
-let a2 = 2;
-let a3 = 3;
-let a4 = 4;
-let a5 = 5;
-let a6 = 6;
-let a7 = 7;
-let a8 = 8;
-let a9 = 9;
-let a0 = 0;
+document.getElementById('n1').addEventListener("click", n1);
+document.getElementById('n2').addEventListener("click", n2);
+document.getElementById('n3').addEventListener("click", n3);
+document.getElementById('n4').addEventListener("click", n4);
+document.getElementById('n5').addEventListener("click", n5);
+document.getElementById('n6').addEventListener("click", n6);
+document.getElementById('n7').addEventListener("click", n7);
+document.getElementById('n8').addEventListener("click", n8);
+document.getElementById('n9').addEventListener("click", n9);
+document.getElementById('n0').addEventListener("click", n0);
+
+document.getElementById('s').addEventListener("click", opS);
+document.getElementById('r').addEventListener("click", opR);
+document.getElementById('m').addEventListener("click", opM);
+document.getElementById('d').addEventListener("click", opD);
 
 function n1() {
-    console.log("hola apretaste el n1");
-    // let numeroX = 1;
-    let actual = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = actual + a1;
+    let actual = document.getElementById("indicador").innerHTML;
+    let sumado = document.getElementById("n1").innerHTML;
+    document.getElementById("indicador").innerHTML = actual + sumado;
 }
+
 function n2() {
-    console.log("hola apretaste el n2");
-    // let a2 = 2;
-    let actual = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = actual + a2;
+    let actual = document.getElementById("indicador").innerHTML;
+    let sumado = document.getElementById("n2").innerHTML;
+    document.getElementById("indicador").innerHTML = actual + sumado;
 }
+
 function n3() {
-    console.log("hola apretaste el n3");
-    // let numeroX = 3;
-    let actual = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = actual + a3;
+    let actual = document.getElementById("indicador").innerHTML;
+    let sumado = document.getElementById("n3").innerHTML;
+    document.getElementById("indicador").innerHTML = actual + sumado;
 }
+
 function n4() {
-    console.log("hola apretaste el n4");
-    // let numeroX = 4;
-    let actual = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = actual + a4;
+    let actual = document.getElementById("indicador").innerHTML;
+    let sumado = document.getElementById("n4").innerHTML;
+    document.getElementById("indicador").innerHTML = actual + sumado;
 }
+
 function n5() {
-    console.log("hola apretaste el n5");
-    // let numeroX = 5;
-    let actual = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = actual + a5;
+    let actual = document.getElementById("indicador").innerHTML;
+    let sumado = document.getElementById("n5").innerHTML;
+    document.getElementById("indicador").innerHTML = actual + sumado;
 }
+
 function n6() {
-    console.log("hola apretaste el n6");
-    // let numeroX = 6;
-    let actual = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = actual + a6;
+    let actual = document.getElementById("indicador").innerHTML;
+    let sumado = document.getElementById("n6").innerHTML;
+    document.getElementById("indicador").innerHTML = actual + sumado;
 }
+
 function n7() {
-    console.log("hola apretaste el n7");
-    // let numeroX = 7;
-    let actual = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = actual + a7;
+    let actual = document.getElementById("indicador").innerHTML;
+    let sumado = document.getElementById("n7").innerHTML;
+    document.getElementById("indicador").innerHTML = actual + sumado;
 }
+
 function n8() {
-    console.log("hola apretaste el n8");
-    // let numeroX = 8;
-    let actual = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = actual + a8;
+    let actual = document.getElementById("indicador").innerHTML;
+    let sumado = document.getElementById("n8").innerHTML;
+    document.getElementById("indicador").innerHTML = actual + sumado;
 }
+
 function n9() {
-    console.log("hola apretaste el n9");
-    // let numeroX = 9;
-    let actual = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = actual + a9;
+    let actual = document.getElementById("indicador").innerHTML;
+    let sumado = document.getElementById("n9").innerHTML;
+    document.getElementById("indicador").innerHTML = actual + sumado;
 }
+
 function n0() {
-    console.log("hola apretaste el n0");
-    // let numeroX = 0;
-    let actual = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = actual + a0;
+    let actual = document.getElementById("indicador").innerHTML;
+    let sumado = document.getElementById("n0").innerHTML;
+    document.getElementById("indicador").innerHTML = actual + sumado;
+}
+// -----------
+
+function opS() {
+    let actual = document.getElementById("indicador").innerHTML;
+    let sumado = document.getElementById("s").innerHTML;
+    document.getElementById("indicador").innerHTML = actual + sumado;
+}
+
+function opR() {
+    let actual = document.getElementById("indicador").innerHTML;
+    let sumado = document.getElementById("r").innerHTML;
+    document.getElementById("indicador").innerHTML = actual + sumado;
+}
+
+function opM() {
+    let actual = document.getElementById("indicador").innerHTML;
+    let sumado = document.getElementById("m").innerHTML;
+    document.getElementById("indicador").innerHTML = actual + sumado;
 }
 
 function opD() {
-    console.log("hola apretaste la division");
-    let operacion = "/";
-    let actual = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = actual + " " + operacion;
+    let actual = document.getElementById("indicador").innerHTML;
+    let sumado = document.getElementById("d").innerHTML;
+    document.getElementById("indicador").innerHTML = actual + sumado;
 }
-function opM() {
-    console.log("hola apretaste la mulsiplicacion");
-    let operacion = "*";
-    let actual = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = actual + " " + operacion;
-}
-function opR() {
-    console.log("hola apretaste la resta");
-    let operacion = "-";
-    let actual = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = actual + " " + operacion;
-}
-function opS() {
-    console.log("hola apretaste la suma");
-    let operacion = "+";
-    let actual = document.getElementById("demo").innerHTML;
-    document.getElementById("demo").innerHTML = actual + " " + operacion;
+
+
+function show() {
+    let actual = document.getElementById("indicador").innerHTML;
+    let suma = actual.indexOf("+");
+    let resta = actual.indexOf("-");
+    let multiplicacion = actual.indexOf("*");
+    let division = actual.indexOf("/");
+
+    if (suma !== -1) {
+        arr = actual.split("+", 2);
+        res = arr[0] + arr[1];
+        document.getElementById("indicador").innerHTML = res;
+    } else if (resta !== -1) {
+        arr = actual.split("-", 2);
+        res = arr[0] - arr[1];
+        document.getElementById("indicador").innerHTML = res;
+    } else if (multiplicacion !== -1) {
+        arr = actual.split("*", 2);
+        res = arr[0] * arr[1];
+        document.getElementById("indicador").innerHTML = res;
+    } else if (division !== -1) {
+        arr = actual.split("/", 2);
+        res = arr[0] / arr[1];
+        document.getElementById("indicador").innerHTML = res;
+    }
+
+
 }
